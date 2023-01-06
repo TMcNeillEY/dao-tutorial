b90e9c42db0614ac2a7726f7c71052b35b282fd9
TMcNeillEY <Tiquila.McNeill@ey.com>
2023-01-06 15:57:19 -0500

    adding the images

--- dao-app-v1/pages/index.js
+++ dao-app-v1/pages/index.js
@@ -373,7 +373,7 @@ export default function Home() {
           {renderTabs()}
         </div>
         <div>
-          <img className={styles.image} src="/cryptodevs/0.svg" />
+          <img className={styles.image} src="cryptodevs/1.svg" />
         </div>
       </div>
 
