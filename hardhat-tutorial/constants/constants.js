87b7d84a05a1664f70634e40ba8acb3c9fd2220e
TMcNeillEY <Tiquila.McNeill@ey.com>
2023-01-06 15:46:08 -0500

    first commit

---
+++ hardhat-tutorial/constants/constants.js
@@ -0,0 +1,8 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x92885a58855e5FBae290f0463a99401d155a9c32";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
+
+// FakeNFTMarketplace deployed to:  0x9Ab0a02AeA763007b730a73b2AF69607c6567D49
+// CryptoDevsDAO deployed to:  0xF182cb66Ac8ADb51250031205109e48460b4E5b6
